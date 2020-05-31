fc35768aeaff24e3e96c623e40cf1fa3c5c8915e
AR2R <mega_4tya@bk.ru>
2020-05-31 20:12:21 +0300

    Ver 2.0

--- src/Field.java
+++ src/Field.java
@@ -1,36 +1,38 @@
 // сущность, описывающая игровое поле
 class Field {
     final static String X = "строки";
-    final static String Y = "столбца";
 
-    String field[][];
+    String field[];
 
     Field() {
-        field = new String[3][3];
-    } // конструктор
-
-    static void initializ(Field ob){ // метод для инициализации поля
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                ob.field[i][j] = "-";
-            }
+        field = new String[9];
+        for (int i = 0; i < field.length; i++) { // Инициализация поля
+                field[i] = "-";
         }
+
     }
 
-    static void printf(Field ob){ // метод для вывода на печать поля
-        System.out.println("__________");
-        for (int i = 0; i < 3; i++) {
-            System.out.print("   ");
-            for (int j = 0; j < 3; j++) {
-                System.out.print(ob.field[i][j]);
+
+    void printf(){ // метод для вывода на печать поля
+        System.out.print("__________");
+        System.out.print("   ");
+            for (int j = 0; j < field.length; j++) {
+//                System.out.print(field[j]);
+                if (j % 3 == 0)
+                    System.out.print("\n   ");
+                System.out.print(field[j]);
             }
-            System.out.println();
-        }
+        System.out.println();
         System.out.println("__________");
     }
 
-    void writeValue(int x, int y ,String value){  // метод для записи значения в поле
-        field[x -1][y -1] = value.toUpperCase();
+    void writeValue(int x, String value){  // метод для записи значения в поле
+        field[x -1] = value.toUpperCase();
+    }
+
+    boolean check(int x){ // метод для проверки заполнено ли в поле значение (на Повтор)
+        if (field[x - 1].equals("-")) return true;
+        else return false;
     }
 
 }
--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,49 +1,128 @@
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
 // содержит бизнес-логику игры
 public class GameLogic {
     static String winValue; // поле для хранения победителя
+    static final String  X = "X";
+    static final String  O = "O";
+    static int count = 0;
+    static final String ERROR = "ВНИМАНИЕ Введено некорректное значение!";
 
-    static boolean check(Field ob, int x, int y){ // метод для проверки заполнено ли в поле значение
-        if (ob.field[x - 1][y - 1].equals("-")) return true;
-        else return false;
-    }
-
-    static boolean winGame(Field ob){ // метод для проверки выйгрыша
-        String mas[][] = ob.field;
-        for (int i = 0; i < 3; i++) {
-            if ((mas[i][0].equals("X")) && (mas[i][1].equals("X")) && (mas[i][2].equals("X"))) {
-                winValue = "X";
-                return true;
-            }
-            if (mas[i][0].equals("O") && mas[i][1].equals("O") && mas[i][2].equals("O")) {
-                winValue = "O";
-                return true;
-            }
-            if (mas[0][i].equals("X") && mas[1][i].equals("X") && mas[2][i].equals("X")) {
-                winValue = "X";
-                return true;
-            }
-            if (mas[0][i].equals("O") && mas[1][i].equals("O") && mas[2][i].equals("O")) {
-                winValue = "O";
-                return true;
-            }
-        }
-        if (mas[0][0].equals("X") && mas[1][1].equals("X") && mas[2][2].equals("X")) {
-            winValue = "X";
-            return true;
-        }
-        if (mas[0][0].equals("O") && mas[1][1].equals("O") && mas[2][2].equals("O")) {
-            winValue = "O";
-            return true;
-        }
-        if (mas[2][0].equals("X") && mas[1][1].equals("X") && mas[0][2].equals("X")) {
-            winValue = "X";
-            return true;
-        }
-        if (mas[2][0].equals("O") && mas[1][1].equals("O") && mas[0][2].equals("O")) {
-            winValue = "O";
-            return true;
-        }
+    boolean winGame(Field ob){ // метод для проверки выйгрыша
+        String mas[] = ob.field;
+        if (trueLine(mas, X)) {winValue = X; return true;}
+        if (trueLine(mas, O)) {winValue = O; return true;}
+        if (trueColumn(mas, X)) {winValue = X; return true;}
+        if (trueColumn(mas, O)) {winValue = O; return true;}
+        if (trueDiag(mas, X)) {winValue = X; return true;}
+        if (trueDiag(mas, O)) {winValue = O; return true;}
         return false;
     }
 
+    boolean trueLine(String ob[],String txt){
+        for (int i = 0; i < 7; i+=3) {
+            if (ob[i].equals(txt) && ob[i + 1].equals(txt) &&
+                    ob[i + 2].equals(txt)) return true;
+        }
+        return false;
+    }
+    boolean trueColumn(String ob[], String txt){
+        for (int i = 0; i < 3; i++) {
+            if (ob[i].equals(txt) && ob[i + 3].equals(txt) && ob[i + 6].equals(txt))
+                return true;
+        }
+        return false;
+    }
+    boolean trueDiag(String ob[], String txt){
+        if (ob[0].equals(txt) && ob[4].equals(txt) && ob[8].equals(txt)) return true;
+        if (ob[2].equals(txt) && ob[4].equals(txt) && ob[6].equals(txt)) return true;
+        return false;
+    }
+
+    void start(Field ob){
+        int x;
+        String value;
+        do {
+            while (true){
+                ob.printf(); // печать поля
+                System.out.println("____________________________");
+                System.out.println("ВВЕСТИ ДАННЫЕ ПЕРВОМУ ИГРОКУ");
+                x = inNumber(); // Вводим номер строки
+                value = inValue(); // Вводим значение Х или О
+                if (ob.check(x)) {
+                    ob.writeValue(x,value);
+                    count++;
+                    break;
+                } else System.out.println("___ДАННОЕ ПОЛЕ ЗАНЯТО___");
+            }
+            if (winGame(ob)) break;
+            if (count == 9) break;
+            while (true) {
+                ob.printf();
+                System.out.println("____________________________");
+                System.out.println("ВВЕСТИ ДАННЫЕ ВТОРОМУ ИГРОКУ");
+                x = inNumber();
+                value = inValue();
+                if (ob.check(x)) {
+                    ob.writeValue(x, value);
+                    count++;
+                    break;
+                } else System.out.println("___ДАННОЕ ПОЛЕ ЗАНЯТО___");
+            }
+        } while (!winGame(ob));
+
+        ob.printf();
+        winText(ob); // Вывод результата игры
+
+
+
+    }
+    int inNumber(){ // Метод для считывания столбца и строки
+        int numbI = 0;
+        boolean istrue = false;
+
+        do {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Введите номер ячейки: от 1 до 9");
+                numbI = scanner.nextInt();
+                if (numbI > 0 && numbI < 10) istrue = true;
+                else throw new IOException();
+            }
+            catch (Exception e){
+                System.out.println(ERROR);
+            }
+        } while (!istrue);
+
+        return numbI;
+    }
+
+    String inValue(){ // Метод для считывания значения Х или О
+        String value = null;
+        boolean istrue = false;
+
+        do {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Введите значение X или O");
+                value = scanner.nextLine();
+                if (value.equalsIgnoreCase("X") || value.equalsIgnoreCase("O")) istrue = true;
+                else throw new IOException();
+            }
+            catch (Exception e){
+                System.out.println(ERROR);
+            }
+        } while (!istrue);
+
+        return value;
+    }
+
+    void winText(Field od){
+        if (winGame(od)) System.out.println("ПОБЕДИЛИ " + winValue + winValue + winValue + "!!!");
+        else if (count == 9) System.out.println("ПОБЕДИЛА ДРУЖБА!!!");
+        else System.out.println("ERROR");
+    }
 }
